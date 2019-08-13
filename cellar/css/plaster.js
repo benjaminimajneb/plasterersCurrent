fc9996bb89f0690a80b409d863b5f33fc3d805fc
benjaminimimajneb <ben@theplasterersarms.co.uk>
2019-08-13 11:10:01 +0100

    Trim old stuff

--- cellar/css/plaster.js
+++
@@ -1,5 +0,0 @@
-$().ready(function() { // once page is loaded, load a random backdrop
-	var i = Math.random();
-	var bgNum=parseInt(i*7)+1;
-	$('#website').css({'background-image':'url(img/back'+bgNum+'.jpg)'});
-});
